1d5341a47e913a6ae6609644500cfece610bf836
Leonardo.Nascimento <BarbosaLeo@github.com>
2020-11-16 23:15:45 -0300

    Update TreeSpawn.cs
    Added some comments

--- TreeSpawn.cs
+++ TreeSpawn.cs
@@ -37,9 +37,15 @@ public class TreeSpawn : MonoBehaviour
 
     public void CastRay(Vector2 point)
     {
+        // Here I have some math that I don't know how to explain in words. It's just to translate the points correcly to fit the chunk size.
+        // Basically, the first terrainchunck V3(0,0,0) is created from the center to the borders, so points will have positive and negative values along the mesh.
+        // The poisson disc is also created from the center but the sample area expands just at positive values.
+        // That means the points location will never have negative values, so it populates only the positive quarter of the chunk.
+        // I did this to solve that problem.
         float dstX = ((transform.position.x - point.x) + terrain.meshSettings.meshWorldSize / 2);
         float dstY = ((transform.position.z - point.y) + terrain.meshSettings.meshWorldSize / 2);
 
+        //we create a new variable Vec2 to set the location properly.
         point = new Vector2(dstX, dstY);
 
         Vector3 raycastPos = new Vector3(point.x, castHeight, point.y);
@@ -47,12 +53,15 @@ public class TreeSpawn : MonoBehaviour
         RaycastHit newPoint;
         LayerMask mask = LayerMask.GetMask("TerrainChunk");
 
+        //Check if we hit the ground.
         if (Physics.Raycast(ray, out newPoint, castHeight+1, mask))
         {
             Debug.DrawLine(ray.origin, newPoint.point, UnityEngine.Color.green);
             
+            //Here we check if the hit point height (Y Axis) is within the range that we set AND if the list does not contain the current point that we are checking.
             if (newPoint.point.y > shoreStartHeight && newPoint.point.y < mountainsStartHeight && !trees.Contains(newPoint.point))
             {
+                //Add a tree element to the object list passing the current point and instantiate the gameObject.
                 trees.Add(newPoint.point);
                 GameObject tree = Instantiate(prefab, newPoint.point, Quaternion.identity);
                 tree.transform.parent = transform;
@@ -60,6 +69,7 @@ public class TreeSpawn : MonoBehaviour
                 tree.layer = 9;
             }
         }
+        //i dont know why but this is here and it works.
         else
         {
         }
